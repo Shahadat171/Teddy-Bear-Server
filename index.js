a4c6e9056e4384ea55bd7b189092187dc6ff0631
Shahadat171 <shahadathosan999@gmail.com>
2023-05-19 01:02:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000 ;
+  
+
+//midleware
+
+app.use(cors())
+app.use(express.json())
+
+app.get( '/', (req, res) =>{
+    res.send('Teddy bear is coming')
+})
+
+app.listen(port, () =>{
+    console.log(`Teddy bear API is running on port : ${port}`)
+})
+
