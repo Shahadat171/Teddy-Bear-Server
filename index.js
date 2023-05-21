93dce6031fa1f4eb836b87f8ef2cd0d98cd10719
Shahadat171 <shahadathosan999@gmail.com>
2023-05-21 13:53:50 +0600

    my toy data added

--- index.js
+++ index.js
@@ -1,19 +1,79 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000 ;
-  
+const port = process.env.PORT || 5000;
+
+const teddyBear = require("./Data/teddyBear.json");
 
 //midleware
 
-app.use(cors())
-app.use(express.json())
+// WDOFlT9lPFfO3qPW
+// teddy-bear
 
-app.get( '/', (req, res) =>{
-    res.send('Teddy bear is coming')
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`Teddy bear API is running on port : ${port}`)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfi6tak.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const teddyBearCollection = client
+      .db("teddybearDB")
+      .collection("teddybear");
+
+    app.get("/teddyBear", async (req, res) => {
+      const cursor = teddyBearCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/myTeddyBear', async (req, res) => {
+      console.log(req.query.sellerEmail)
+      let query = {};
+      if (req.query?.sellerEmail) {
+          query = { sellerEmail: req.query.sellerEmail }
+      }
+      const result = await teddyBearCollection.find(query).toArray();
+      res.send(result);
+  })
+
+    app.post("/teddyBear", async (req, res) => {
+      const newTeddyBear = req.body;
+      console.log(newTeddyBear);
+      const result = await teddyBearCollection.insertOne(newTeddyBear);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Teddy bear is coming");
+});
+
+app.listen(port, () => {
+  console.log(`Teddy bear API is running on port : ${port}`);
+});
